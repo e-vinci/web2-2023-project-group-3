2a6468fba9a57e8751484cb41cab57597bee47ec
antoine <antoine.drionduchapoi@student.vinci.be>
2023-11-14 15:33:24 +0100

    quick fix

--- api/models/Admin.js
+++ api/models/Admin.js
@@ -1,7 +1,7 @@
 // eslint-disable-next-line import/extensions
 const db = require('./db_conf.js');
 
-module.exports.All_Order = () => {
+module.exports.allOrders = () => {
   const allOrders = db.prepare('SELECT * FROM commandes').all();
   return allOrders.all();
 };
