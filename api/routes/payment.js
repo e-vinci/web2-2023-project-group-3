b208e26562a8d31da3116a9398cd575629ef5519
antoine <antoine.drionduchapoi@student.vinci.be>
2023-11-30 20:14:07 +0100

    stripe changes

--- api/routes/payment.js
+++ api/routes/payment.js
@@ -8,18 +8,18 @@ const router = express.Router();
 const config = require('../config');
 const stripe = require('stripe')(config.stripe_key);
 
-const { allOrdersFromUser } = Payment;
-
 // Define your payment routes here...
 
 router.post('/checkout', async (req, res) => {
   /// mettre query pour ajouter info dans db en fonction info client
 
-  const nom = req?.body?.name?.length !== 0 ? req.body.name : undefined;
-  const description = req?.body?.description?.length !== 0 ? req.body.description : undefined;
-  const prixUnitaire = req?.body?.prix_unitaire?.length !== 0 ? req.body.prix_unitaire : undefined;
-  const type = req?.body?.type?.length !== 0 ? req.body.type : undefined;
-  console.log(nom, description, prixUnitaire, type);
+  const orderFromUser = Payment.allOrdersFromUser(1);
+  const price = orderFromUser.prix_total;
+  const name = orderFromUser.nom;
+  const firstname = orderFromUser.prenom;
+
+  console.log(name + ' ' + firstname + ' vient de faire une commande pour : ' + price + '€');
+
   try {
     // Fetch product details from the database
 
@@ -32,8 +32,8 @@ router.post('/checkout', async (req, res) => {
           price_data: {
             currency: 'eur',
             product_data: {
-              name: 'Sushi',
-              description: 'Sushi',
+              name: 'sushi',
+              description: 'sushi',
 
               // Add any other relevant product details from your database
             },
@@ -45,7 +45,7 @@ router.post('/checkout', async (req, res) => {
       mode: 'payment',
 
       success_url: 'http://localhost:3000/payment/succes', // Replace with your success URL
-      cancel_url: 'http://localhost:3000/', // Replace with your cancel URL
+      cancel_url: 'http://localhost:3000/payment/error', // Replace with your cancel URL
     });
 
     // Redirect the user to the Stripe Checkout page
@@ -58,22 +58,29 @@ router.post('/checkout', async (req, res) => {
 
 router.get('/succes', async (req, res) => {
   console.log('succes');
-  const order = allOrdersFromUser(1);
 
-  return res.json(order);
+  return res.json();
+});
+
+router.get('/error', async (req, res) => {
+  console.log('error');
+
+  res.send('POST request to the checkout page');
+  return res.json();
 });
 
 router.get('/test', async (req, res) => {
   console.log('succes');
-  const price = Payment.allOrdersFromUser(1).prix_total;
-
- 
-
 
   //do that for every object of the array
-  console.log(price);
+  const orderFromUser = Payment.allOrdersFromUser(1);
+  const price = orderFromUser.prix_total;
+  const name = orderFromUser.nom;
+  const firstname = orderFromUser.prenom;
 
-  return res.json(price);
+  console.log(name + '' + firstname + ' vient de faire une commande pour : ' + price + '€');
+
+  return res.json();
 });
 
 module.exports = router;
