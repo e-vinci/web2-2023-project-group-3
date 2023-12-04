2417908f1a1a1c8550b8fb0a69a47fb26dccbf86
Manal <manal.hebbal@student.vinci.be>
2023-12-04 13:54:50 +0100

    refactor

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -45,33 +45,26 @@ const Navbar = () => {
   `;
   const navbarAuth = `
 
-<<<<<<< HEAD
   <div id="headerLogo" >
   <nav class="navbar navbar-expand-lg style="background-color: #e3f2fd;"">
   <div class="container-fluid">
-  <a class="navbar-brand justify-content-center" href="#">
-  <img src="${logo}" alt="Logo" width="120" height="95" class="d-inline-block align-text-top ">
-</a>
+  
+  <img src="${logo}" alt="Logo" width="170" height="85" class="d-inline-block align-text-top " id="imgHeader">
+
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-        <button id="connexion" href="#" data-uri="/card"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-fill" viewBox="0 0 16 16">
-        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-      </svg></button> 
-        <button id="inscription" href="#" data-uri="/deconnexion">Se deconnecter</button>
+        <button id="connexion" href="#" data-uri="/deconnexion"> Se deconnecter</button> 
+      
       </div>
       </div>
     </div>
   </div>
 </nav>
   
-=======
-  <div id="headerLogo">
-    <img id="imgHeader" src="${logo}" alt="" style="width:15%; height:auto"></a>
-    <button id="connexion" href="#" data-uri="/logout">Se deconnecter</button>
->>>>>>> ff1972efa42abfbc477306a5d07d516d86711483
     </div>
 
   <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #343A40;">
