14f2560e32531a417c655a25b033e1d1a1e12ad1
Manal <manal.hebbal@student.vinci.be>
2023-12-09 21:44:56 +0100

    menu page list of card

--- frontend/src/Components/Pages/Menu.js
+++ frontend/src/Components/Pages/Menu.js
@@ -1,54 +1,173 @@
-import royalBox from '../../img/sushiRoyalBox.png'
+// import royalBox from '../../img/sushiRoyalBox.png'
+import sushi1 from "../../img/sushis/1.png"
 
 const Menu = () => {
     const main = document.querySelector('main');
 
     const bloc1= `
-    <div class="menu">
-      <div class="filter">
-        <div class="filter-card">
-          <a href=# >MAKI</a>
-        </div>  
-
-        <div class="filter-card">
-         <a href=# >CALIFORNIA ROLL</a>
-        </div> 
-
-        <div class="filter-card">
-          <a href=# >SAUMON ROLL</a>
-        </div> 
-
-        <div class="filter-card">
-          <a href=# >CRUSTY</a>
-        </div> 
-
-        <div class="filter-card">
-          <a href=# >NIGIRI</a>
-        </div> 
-
-        <div class="filter-card">
-          <a href=# >NOS BOXES</a>
-        </div> 
-       
-      </div>
-
-      <div id="cardBox2">
-      <div class="card mb-3" style="max-width: 1200px; ">
-        <div class="row g-0">
-          <div class="col-md-3">
-            <img src="${royalBox}" class="img-fluid rounded-start" alt="...">
-          </div>
-          <div class="col-md-6">
-            <div class="card-body">
-              <p id="cardBoxContent">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </p>
-              
-            </div>
-            <button id="buttonBox2" href="#" data-uri="/creationBox" >Ajouter au panier</button>
-            <button id="buttonBox3" href="#" data-uri="/creationBox" >Annuler</button>
-          </div>
-       </div>
+    <div class="text-center" style="padding-top: 2%; padding-bottom: 2%; background-color: #151313;">
+      <h1 style="font-weight:700; color: #C69751;" >FAITES VOTRE CHOIX!</h1>
     </div>
 
+    <div class="menu">
+      <div class="text-center">
+        <button class="btn btn-small" style="background-color: #C69751; width: 15%;" data-toggle="portfilter" data-target="all">
+          MAKI
+        </button>
+        <button class="btn btn-small" style="background-color: #C69751; width: 15%;" data-toggle="portfilter" data-target="art">
+          CALIFORNIA ROLLS
+        </button>
+        <button class="btn btn-small" style="background-color: #C69751; width: 15%;" data-toggle="portfilter" data-target="media">
+          SALMON ROLLS
+        </button>
+        <button class="btn btn-small" style="background-color: #C69751; width: 15%;" data-toggle="portfilter" data-target="brand">
+          CRUSTY
+        </button>
+      </div>
+    </div>
+
+    <div class="products">
+      <div class="cardMenu">
+                          
+              <div class="card-image">
+                      <img src="${sushi1}" width="150"> 
+              </div>  
+
+              <div class="card-inner">
+                  <span></span> 
+                  <h5 class="mb-0">Maki Saumon</h5> 
+
+                  <div class="price">
+                      <span>7.99€</span>
+                  </div>
+
+
+                  <div class="mt-3 d-flex justify-content-between align-items-center">
+                      <button class="btn text-uppercase btn-sm details" style="background-color: #C69751;" >Details</button>
+                      <div class="d-flex flex-row">
+                          <span class="cart"><i class="fa fa-shopping-cart">+</i></span>  
+                      </div>
+                  </div>
+              </div>                    
+      </div>
+
+      <div class="cardMenu">
+                          
+              <div class="card-image">
+                      <img src="${sushi1}" width="150"> 
+              </div>  
+
+              <div class="card-inner">
+                  <span></span> 
+                  <h5 class="mb-0">Maki Saumon</h5> 
+
+                  <div class="price">
+                      <span>7.99€</span>
+                  </div>
+
+
+                  <div class="mt-3 d-flex justify-content-between align-items-center">
+                      <button class="btn text-uppercase btn-sm details" style="background-color: #C69751;" >Details</button>
+                      <div class="d-flex flex-row">
+                          <span class="cart"><i class="fa fa-shopping-cart">+</i></span>  
+                      </div>
+                  </div>
+              </div>                    
+      </div>
+
+      <div class="cardMenu">
+                          
+              <div class="card-image">
+                      <img src="${sushi1}" width="150"> 
+              </div>  
+
+              <div class="card-inner">
+                  <span></span> 
+                  <h5 class="mb-0">Maki Saumon</h5> 
+
+                  <div class="price">
+                      <span>7.99€</span>
+                  </div>
+
+
+                  <div class="mt-3 d-flex justify-content-between align-items-center">
+                      <button class="btn text-uppercase btn-sm details" style="background-color: #C69751;" >Details</button>
+                      <div class="d-flex flex-row">
+                          <span class="cart"><i class="fa fa-shopping-cart">+</i></span>  
+                      </div>
+                  </div>
+              </div>                    
+      </div>
+      
+      <div class="cardMenu">
+                          
+          <div class="card-image">
+                  <img src="${sushi1}" width="150"> 
+          </div>  
+
+          <div class="card-inner">
+              <span></span> 
+              <h5 class="mb-0">Maki Saumon</h5> 
+
+              <div class="price">
+                  <span>7.99€</span>
+              </div>
+
+
+              <div class="mt-3 d-flex justify-content-between align-items-center">
+                  <button class="btn text-uppercase btn-sm details" style="background-color: #C69751;" >Details</button>
+                  <div class="d-flex flex-row">
+                      <span class="cart"><i class="fa fa-shopping-cart">+</i></span>  
+                  </div>
+              </div>
+          </div>                    
+      </div>
+        <div class="cardMenu">
+                                  
+              <div class="card-image">
+                      <img src="${sushi1}" width="150"> 
+              </div>  
+
+              <div class="card-inner">
+                  <span></span> 
+                  <h5 class="mb-0">Maki Saumon</h5> 
+
+                  <div class="price">
+                      <span>7.99€</span>
+                  </div>
+
+
+                  <div class="mt-3 d-flex justify-content-between align-items-center">
+                      <button class="btn text-uppercase btn-sm details" style="background-color: #C69751;" >Details</button>
+                      <div class="d-flex flex-row">
+                          <span class="cart"><i class="fa fa-shopping-cart">+</i></span>  
+                      </div>
+                  </div>
+              </div>                    
+        </div>
+      <div class="cardMenu">
+                                
+            <div class="card-image">
+                    <img src="${sushi1}" width="150"> 
+            </div>  
+
+            <div class="card-inner">
+                <span></span> 
+                <h5 class="mb-0">Maki Saumon</h5> 
+
+                <div class="price">
+                    <span>7.99€</span>
+                </div>
+
+
+                <div class="mt-3 d-flex justify-content-between align-items-center">
+                    <button class="btn text-uppercase btn-sm details" style="background-color: #C69751;" >Details</button>
+                    <div class="d-flex flex-row">
+                        <span class="cart"><i class="fa fa-shopping-cart">+</i></span>  
+                    </div>
+                </div>
+            </div>                    
+      </div>
     </div>
     `
 
