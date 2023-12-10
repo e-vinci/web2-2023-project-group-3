0876a7139d2e8d3605387083e6c10db03f5e9544
Didia <didia.josefechado@student.vinci.be>
2023-12-10 15:13:07 +0100

    lien front back menu

--- frontend/src/Components/Pages/Menu.js
+++ frontend/src/Components/Pages/Menu.js
@@ -1,5 +1,60 @@
 import royalBox from '../../img/sushiRoyalBox.png'
 
+// Déclarez une variable globale pour stocker tous les sushis
+let allSushis = [];
+
+// Fonction pour afficher les sushis dans le DOM
+const displaySushis = (sushis) => {
+  const main = document.querySelector('main');
+  // eslint-disable-next-line no-unused-vars
+  const menu = document.querySelector('.menu');
+
+  // Créez une section pour afficher les sushis
+  const sushiSection = document.createElement('div');
+  sushiSection.classList.add('sushi-section');
+
+  // Ajoutez chaque sushi à la section
+  sushis.forEach(sushi => {
+    const sushiCard = document.createElement('div');
+    sushiCard.classList.add('sushi-card');
+    sushiCard.innerHTML = `
+      <h3>${sushi.nom}</h3>
+      <p>Description: ${sushi.description}</p>
+      <p>Prix: ${sushi.prix_unitaire}</p>
+    `;
+    sushiSection.appendChild(sushiCard);
+  });
+
+  // Ajoutez la section des sushis au DOM
+  main.appendChild(sushiSection);
+};
+
+// Fonction pour filtrer les sushis par type
+window.filterSushis = (type) => {
+    console.log(`Filtrage des sushis par type : ${type}`);
+  const filteredSushis = allSushis.filter(sushi => sushi.type === type);
+  console.log(filteredSushis);
+  displaySushis(filteredSushis);
+};
+
+const menu = async () => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const response = await fetch('http://localhost:3000/sushis', options);
+  console.log(response.status);
+
+  if (response.ok) {
+    allSushis = await response.json();
+    displaySushis(allSushis);
+  } else {
+    console.error('Erreur lors de la récupération du menu.');
+  }
+};
+
 const Menu = () => {
     const main = document.querySelector('main');
 
@@ -7,23 +62,23 @@ const Menu = () => {
     <div class="menu">
       <div class="filter">
         <div class="filter-card">
-          <a href=# >MAKI</a>
+          <a href=# onclick="filterSushis('Maki')">MAKI</a>
         </div>  
 
         <div class="filter-card">
-         <a href=# >CALIFORNIA ROLL</a>
+         <a href=# onclick="filterSushis('CaliforniaRoll')">CALIFORNIA ROLL</a>
         </div> 
 
         <div class="filter-card">
-          <a href=# >SAUMON ROLL</a>
+          <a href=# onclick="filterSushis('SaumonRoll')">SAUMON ROLL</a>
         </div> 
 
         <div class="filter-card">
-          <a href=# >CRUSTY</a>
+          <a href=# onclick="filterSushis('Crusty')">CRUSTY</a>
         </div> 
 
         <div class="filter-card">
-          <a href=# >NIGIRI</a>
+          <a href=# onclick="filterSushis('Nigiri')">NIGIRI</a>
         </div> 
 
         <div class="filter-card">
@@ -47,12 +102,14 @@ const Menu = () => {
             <button id="buttonBox3" href="#" data-uri="/creationBox" >Annuler</button>
           </div>
        </div>
+       <div class="sushi-section"></div>
     </div>
 
     </div>
     `
 
     main.innerHTML = bloc1;
+    menu();
   };
   
   export default Menu;
