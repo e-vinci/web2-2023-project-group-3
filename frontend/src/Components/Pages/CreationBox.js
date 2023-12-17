429e65bc18fafaeec61ef4f65478e5498086a4dc
antoine <antoine.drionduchapoi@student.vinci.be>
2023-12-17 08:53:55 +0100

    creationBox fix method

--- frontend/src/Components/Pages/CreationBox.js
+++ frontend/src/Components/Pages/CreationBox.js
@@ -1,18 +1,17 @@
-import boite from '../../img/boite.png'
-import sushi1 from '../../img/sushis/1.png'
-import s1 from "../../img/sushis/1bis.png";
-import s2 from "../../img/sushis/2bis.png";
-import s3 from "../../img/sushis/3bis.png";
-import s4 from "../../img/sushis/4bis.png";
-import s5 from "../../img/sushis/5.png";
-import s6 from "../../img/sushis/6.png";
-import s7 from "../../img/sushis/7.png";
-import s8 from "../../img/sushis/8.png";
-import s9 from "../../img/sushis/9.png";
-import s10 from "../../img/sushis/10.png";
+import boite from '../../img/boite.png';
+import sushi1 from '../../img/sushis/1.png';
+import s1 from '../../img/sushis/1bis.png';
+import s2 from '../../img/sushis/2bis.png';
+import s3 from '../../img/sushis/3bis.png';
+import s4 from '../../img/sushis/4bis.png';
+import s5 from '../../img/sushis/5.png';
+import s6 from '../../img/sushis/6.png';
+import s7 from '../../img/sushis/7.png';
+import s8 from '../../img/sushis/8.png';
+import s9 from '../../img/sushis/9.png';
+import s10 from '../../img/sushis/10.png';
 
-
-const allPics =[s1,s2,s3,s4,s5,s6,s7,s8,s9,s10];
+const allPics = [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10];
 
 // Déclarez une variable globale pour stocker tous les sushis
 let allSushis = [];
@@ -20,7 +19,7 @@ let allSushis = [];
 const CreationBox = () => {
   const main = document.querySelector('main');
 
-  const bloc1= `
+  const bloc1 = `
   <div class="text-center" style="padding-top: 2%; padding-bottom: 2%; background-color: #151313;">
     <h1 style="font-weight:700; color: #C69751;" >Créez votre propre box personnalisée!</h1>
   </div>
@@ -47,9 +46,11 @@ const CreationBox = () => {
             
         
             <form action="http://localhost:8080/creationBox" method="get">
-              <button id="ajouterPanier"  data-uri="/creationBox" onclick="creationBox(); afficherAnimation;" >Ajouter au panier</button>
+              <button id="ajouterPanier"  data-uri="/creationBox"  onclick="creationBox(); afficherAnimation;">Ajouter au panier</button>
               <button id="annulerBox" href="#" data-uri="/creationBox" >Annuler</button>
             </form>      
+
+            
 </div>
 
           
@@ -57,14 +58,12 @@ const CreationBox = () => {
            
    
 
-    `
-    const displaySushis = (sushis) => {
-
-        const products = document.createElement('div');
-        products.classList.add('products');
-          sushis.forEach((sushi, index) => {
-            products.innerHTML+=
-            `
+    `;
+  const displaySushis = (sushis) => {
+    const products = document.createElement('div');
+    products.classList.add('products');
+    sushis.forEach((sushi, index) => {
+      products.innerHTML += `
             <div class="cardMenu">
                                 
             <div class="card-image">
@@ -76,7 +75,7 @@ const CreationBox = () => {
                 <h5 class="mb-0">${sushi.nom}</h5> 
       
                 <div class="price">
-                    <span>${sushi.prix_unitaire }€/piece</span>
+                    <span>${sushi.prix_unitaire}€/piece</span>
                 </div>
       
       
@@ -104,138 +103,120 @@ const CreationBox = () => {
                 </div>                    
           </div>
     
-          `
-      
-          });   
-          
-          products.innerHTML +=     `
+          `;
+    });
+
+    products.innerHTML += `
           <div class="text-center" style="padding-top: 2%; padding-bottom: 2%; background-color: #151313;">
             <h1 style="font-weight:700; color: #C69751;" > OU TROUVEZ VOTRE BONHEUR PARMIS NOS BOXS:</h1>
           </div>
-          `
-          main.appendChild(products);
-       
-      
-        }
-          const menu = async () => {
-            const options = {
-              method: 'GET',
-              headers: {
-                'Content-Type': 'application/json',
-              },
-            };
-            const response = await fetch('http://localhost:3000/sushis', options);
-            console.log(response.status);
-      
-            if (response.ok) {
-              allSushis = await response.json();
-              displaySushis(allSushis);
-              console.log(allSushis);
-            } else {
-              console.error('Erreur lors de la récupération du menu.');
-            }
-          };
-      
-          // Fonction pour filtrer les sushis par type
-            window.filterSushis = (type) => {
-              console.log(`Filtrage des sushis par type : ${type}`);
-            const filteredSushis = allSushis.filter(sushi => sushi.type === type);
-            console.log(filteredSushis);
-            displaySushis(filteredSushis);
-            };
+          `;
+    main.appendChild(products);
+  };
+  const menu = async () => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch('http://localhost:3000/sushis', options);
+    console.log(response.status);
 
-            
-      
-          main.innerHTML = bloc1;
-          menu();
-      
+    if (response.ok) {
+      allSushis = await response.json();
+      displaySushis(allSushis);
+      console.log(allSushis);
+    } else {
+      console.error('Erreur lors de la récupération du menu.');
+    }
+  };
 
-          window.afficher = async () => {
-            document.getElementById('modal').style.display = 'block'
-          };
+  // Fonction pour filtrer les sushis par type
+  window.filterSushis = (type) => {
+    console.log(`Filtrage des sushis par type : ${type}`);
+    const filteredSushis = allSushis.filter((sushi) => sushi.type === type);
+    console.log(filteredSushis);
+    displaySushis(filteredSushis);
+  };
 
-          window.afficherAnimation = async() => {       
-            
-            const boiteImage = document.querySelector('.boxnew img');
-          
-            // Ajoutez la classe 'shake' à l'image de la boîte pour l'animation
-            boiteImage.classList.add('shake');
-          
-            // Supprimez la classe 'shake' après 500 ms (0.5s)
-            setTimeout(() => {
-              boiteImage.classList.remove('shake');
-            }, 500);
-            const options = {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-          };
-            const response = await fetch('http://localhost:3000/sushis', options);
-            console.log(response.status);
-      
-          if (response.ok) {
-              console.log('ok')
-          } else {
-              console.error('Erreur lors de l ajout à la box.');
-          }
+  main.innerHTML = bloc1;
+  menu();
 
-     
-          };
+  window.afficher = async () => {
+    document.getElementById('modal').style.display = 'block';
+  };
 
-          window.creationBox = async () => {
-            try {
-                const response = await fetch('http://localhost:3000/sushis/creationBox');
-        
-                console.log('Response status:', response.status);
-        
-                if (response.ok) {
-                    const responseData = await response.json();
-                    console.log('Response data:', responseData);
-                    // Handle the response data as needed
-                } else {
-                    console.error('Error during GET request.');
-                }
-            } catch (error) {
-                console.error('Fetch error:', error.message);
-            }
-        };
-        
+  window.afficherAnimation = async () => {
+    const boiteImage = document.querySelector('.boxnew img');
 
+    // Ajoutez la classe 'shake' à l'image de la boîte pour l'animation
+    boiteImage.classList.add('shake');
 
-          window.ajouterSushi = async (idSushi) => {
-           
-            const options = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'sushi-id': idSushi,
-                },
-                
-            };
-        
-            try {
-                const response = await fetch('http://localhost:3000/sushis/ajouterSushi', options);
-                console.log('Response status:', response.status);
-        
-                if (response.ok) {
-                    console.log('Sushi added successfully');
-                    console.log(idSushi);
-                    const responseData = await response.json();
-                    console.log('Response data:', responseData);
-                } else {
-                    console.error('Error adding sushi to the box.');
-                }
-            } catch (error) {
-                console.error('Fetch error:', error.message);
-            }
-        };
-        
+    // Supprimez la classe 'shake' après 500 ms (0.5s)
+    setTimeout(() => {
+      boiteImage.classList.remove('shake');
+    }, 500);
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch('http://localhost:3000/sushis', options);
+    console.log(response.status);
+
+    if (response.ok) {
+      console.log('ok');
+    } else {
+      console.error('Erreur lors de l ajout à la box.');
+    }
+  };
+
+  window.creationBox = async () => {
+    console.log(' ! CREATION BOX ! ');
+
+    try {
+      const response = await fetch('http://localhost:3000/sushis/creationBox');
+      console.log('Response status:', response.status);
+
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Response data:', responseData);
+      } else {
+        console.error('Errorcreating the box.');
+      }
+    } catch (error) {
+      console.error('Fetch error:', error.message);
+    }
+  };
+
+  window.ajouterSushi = async (idSushi) => {
+    console.log(' ! AJOUT SUSHI ! ');
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'sushi-id': idSushi,
+      },
+    };
+
+    try {
+      const response = await fetch('http://localhost:3000/sushis/ajouterSushi', options);
+      console.log('Response status:', response.status);
+
+      if (response.ok) {
+        console.log('Sushi added successfully');
+        console.log(idSushi);
+        const responseData = await response.json();
+        console.log('Response data:', responseData);
+      } else {
+        console.error('Error adding sushi to the box.');
+      }
+    } catch (error) {
+      console.error('Fetch error:', error.message);
+    }
+  };
 };
-
-      
-
-
-
 
 export default CreationBox;
